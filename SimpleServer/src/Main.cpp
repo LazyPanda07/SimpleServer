2b8d5d34f3bf79f8c8dd85a099acaf4d82fbb62d
LazyPanda07 <semengricenko@gmail.com>
2022-06-16 15:24:29 +0500

    Change subsystem

--- SimpleServer/src/Main.cpp
+++ SimpleServer/src/Main.cpp
@@ -1,7 +1,26 @@
+#include "headers.h"
 
-int main(int argc, char** argv)
+#include "GUIFramework.h"
+#include "Exceptions/BaseGUIFrameworkException.h"
+
+using namespace std;
+
+#pragma warning(disable: 28251)
+
+int wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
 {
-	
+	try
+	{
+		gui_framework::GUIFramework& instance = gui_framework::GUIFramework::get();
+	}
+	catch (const gui_framework::exceptions::BaseGUIFrameworkException& e)
+	{
+		// TODO: show error
+	}
+	catch (const exception& e)
+	{
+		// TODO: show error
+	}
 
 	return 0;
 }
