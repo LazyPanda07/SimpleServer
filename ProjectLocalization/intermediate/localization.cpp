91354bcef6e38388d7392b67c18a1117f16e5239
LazyPanda07 <semengricenko@gmail.com>
2022-06-16 14:41:21 +0500

    Add WebFramework

---
+++ ProjectLocalization/intermediate/localization.cpp
@@ -0,0 +1,7 @@
+#include <unordered_map>
+
+using namespace std;
+
+#define LOCALIZATION_API extern "C" __declspec(dllexport)
+
+LOCALIZATION_API const string originalLanguage = "en";
--- SimpleServer/src/Main.cpp
+++ SimpleServer/src/Main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+#include "WebFramework.h"
+#include "Exceptions/BaseWebFrameworkException.h"
+
 using namespace std;
 
 int main(int argc, char** argv)
 {
-
-
+	
 
 	return 0;
 }
